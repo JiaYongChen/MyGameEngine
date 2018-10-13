dc467670c3d3819db9294f47939c307fc9d19ad9
ChenJiaYong <cjy461549601@163.com>
2018-10-13 14:14:31 +0800

    mv mac to test and add graphicsmanager

---
+++ Framework/Common/GraphicsManager.hpp
@@ -0,0 +1,13 @@
+#ifndef _GRAPHICSMANAGER_HPP_
+#define _GRAPHICSMANAGER_HPP_
+
+#include "../Interface/IRuntimeModule.hpp"
+
+namespace MyGame{
+    class GraphisManager : implements IRuntimeModule{
+        public:
+            virtual ~GraphisManager() {}
+    };
+}
+
+#endif
