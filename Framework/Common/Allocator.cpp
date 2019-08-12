2e4bc29645bdcaab22127b6290d65a1da14bb9d5
cjy <cjy461549601@hotmail.com>
2019-08-12 22:26:21 +0800

    add allocator

--- Framework/Common/Allocator.cpp
+++ Framework/Common/Allocator.cpp
@@ -40,5 +40,111 @@ void GameEngine::Allocator::Reset(size_t dataSize, size_t pageSize, size_t align
 
 void* GameEngine::Allocator::Allocate(void)
 {
-    
+    if(!m_pFreeList){
+        //allocate a new page
+        PageHeader* pNewPage = reinterpret_cast<PageHeader*>(new uint8_t[m_szPageSize]);
+        ++m_nPages;
+        m_nBlocks += m_nBlocksPerPage;
+        m_nFreeBlocks += m_nBlocksPerPage;
+
+#if defined(_DEBUG)
+        FillFreePage(pNewPage);
+#endif
+        if(m_pPageList){
+            pNewPage->pNext = m_pPageList;
+        }
+
+        m_pPageList = pNewPage;
+
+        BlockHeader* pBlock = pNewPage->Blocks();
+        //link each block in the page
+        for (uint32_t i = 0; i < m_nBlocksPerPage; ++i){
+            pBlock->pNext = NextBlock(pBlock);
+            pBlock = NextBlock(pBlock);
+        }
+        pBlock->pNext = nullptr;
+
+        m_pFreeList = pNewPage->Blocks();
+    }
+
+    BlockHeader* freeBlock = m_pFreeList;
+    m_pFreeList = m_pFreeList->pNext;
+    --m_nFreeBlocks;
+
+#if defined(_DEBUG)
+    FillAllocatedBlock(freeBlock);
+#endif
+
+    return reinterpret_cast<void*>(freeBlock);
+}
+
+void GameEngine::Allocator::Free(void* p)
+{
+    BlockHeader* block = reinterpret_cast<BlockHeader*>(p);
+
+#if defined(_DEBUG)
+    FillAllocatedBlock(block);
+#endif
+
+    block->pNext = m_pFreeList;
+    m_pFreeList = block;
+    ++m_nFreeBlocks;
+}
+
+void GameEngine::Allocator::FreeAll()
+{
+    PageHeader* pPage = m_pPageList;
+    while (pPage)
+    {
+        PageHeader* _p = pPage;
+        pPage = pPage->pNext;
+
+        delete [] reinterpret_cast<uint32_t*>(_p);
+    }
+
+    m_pPageList = nullptr;
+    m_pFreeList = nullptr;
+
+    m_nPages = 0;
+    m_nBlocks = 0;
+    m_nFreeBlocks = 0;
+}
+
+#if defined(_DEBUG)
+void GameEngine::Allocator::FillFreePage(PageHeader* pPage)
+{
+    //page header
+    pPage->pNext = nullptr;
+
+    //blocks
+    BlockHeader* pBlock = pPage->Blocks();
+    for (uint32_t i = 0; i < m_nBlocksPerPage; i++)
+    {
+        FillFreeBlock(pBlock);
+        pBlock = NextBlock(pBlock);
+    }
+}
+
+void GameEngine::Allocator::FillFreeBlock(BlockHeader* pBlock)
+{
+    //block header + data
+    std::memset(pBlock, PATTERN_FREE, m_szBlockSize - m_szAlignmentSize);
+
+    //alignment
+    std::memset(reinterpret_cast<uint8_t*>(pBlock) + m_szBlockSize - m_szAlignmentSize, PATTERN_ALIGN, m_szAlignmentSize);
+}
+
+void GameEngine::Allocator::FillAllocatedBlock(BlockHeader *pBlock)
+{
+    //block header + data
+    std::memset(pBlock, PATTERN_ALLOC, m_szBlockSize - m_szAlignmentSize);
+
+    //alignment
+    std::memset(reinterpret_cast<uint8_t*>(pBlock) + m_szBlockSize - m_szAlignmentSize, PATTERN_ALIGN, m_szAlignmentSize);
+}
+#endif
+
+GameEngine::BlockHeader* GameEngine::Allocator::NextBlock(BlockHeader* pBlock)
+{
+    return reinterpret_cast<BlockHeader*>(reinterpret_cast<uint8_t*>(pBlock) + m_szBlockSize);
 }
